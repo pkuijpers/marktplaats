4f04640ba6b72aaddfbee7ee6542d94da8829f5a
Pieter Kuijpers <pieter@pi-q.nl>
2013-07-28 14:25:50 +0200

    Added Item class

--- lib/marktplaats.rb
+++ lib/marktplaats.rb
@@ -6,7 +6,11 @@ class Marktplaats
     agent = Mechanize.new
     page = agent.get('http://www.marktplaats.nl/z/computers-en-software/videokaarten.html?categoryId=353')
     listings = page.search('.search-result')
-    items = listings.map {|listing| listing.at('.mp-listing-title').text }
+    items = listings.map do |listing| 
+      Marktplaats::Item.new(listing.at('.mp-listing-title').text)
+    end
   end
 
 end
+
+require 'item'
--- spec/marktplaats_spec.rb
+++ spec/marktplaats_spec.rb
@@ -1,9 +1,18 @@
 require './lib/marktplaats.rb'
 
-describe Marktplaats, "#video_cards" do
-  it "returns a list of video cards available on marktplaats" do
-    marktplaats = Marktplaats.new
-    items = marktplaats.get_category("Videokaarten")
-    expect(items.size).to be > 0
+describe Marktplaats, "#get_category" do
+  before do
+    @marktplaats = Marktplaats.new
+    @items = @marktplaats.get_category("Videokaarten")
+  end
+
+  it "returns a list of items available on marktplaats" do
+    expect(@items.size).to have_at_least(1).item
+  end
+
+  it "returns items with a title" do
+    @items.each do |item|
+      expect(item.title).not_to be_nil
+    end
   end
 end
