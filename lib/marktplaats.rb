41f220afbd3c9f24ebc1f07361b20423677a8fc0
Pieter Kuijpers <pieter@pi-q.nl>
2013-08-17 12:39:12 +0200

    Added search function

--- lib/marktplaats.rb
+++ lib/marktplaats.rb
@@ -1,15 +1,26 @@
 require 'mechanize'
+require 'open-uri'
 
 class Marktplaats
 
-  URL = 'http://www.marktplaats.nl/z/cd-s-en-dvd-s/vinyl-singles.html?categoryId=1380'
-# URL = 'http://www.marktplaats.nl/z/computers-en-software/videokaarten.html?categoryId=353'
 
   def get_category(category, max_results: 200)
-    agent = Mechanize.new
+    url = 'http://www.marktplaats.nl/z/cd-s-en-dvd-s/vinyl-singles.html?categoryId=1380'
+    # url = 'http://www.marktplaats.nl/z/computers-en-software/videokaarten.html?categoryId=353'
+    get_items(url, max_results)
+  end
 
+  def search(search_term)
+    search_url = "http://www.marktplaats.nl/z.html?query=#{URI::encode(search_term)}"
+    get_items(search_url, 100)
+  end
+
+  private
+
+  def get_items(url, max_results)
+    agent = Mechanize.new
     # Load first page
-    page = agent.get(URL)
+    page = agent.get(url)
     items = items_from(page)
   
     next_button = page.link_with(:text => 'Volgende')
@@ -21,13 +32,17 @@ class Marktplaats
     items
   end
 
-  private
-
   def items_from(page)
     listings = page.search('.search-result').to_ary
+
     # Bottom listing contains advertisements, don't include them
     listings.reject! do |listing|
       listing.attributes['class'].value.include?('bottom-listing')
+    end
+
+    # Ignore professional sellers: items that have a 'seller-link'
+    listings.reject! do |listing|
+      listing.at('.seller-link') != nil
     end
 
     listings.map do |listing| 
--- spec/marktplaats_spec.rb
+++ spec/marktplaats_spec.rb
@@ -1,21 +1,35 @@
 require './lib/marktplaats.rb'
 
-describe Marktplaats, "#get_category" do
+describe Marktplaats do
   before do
     @marktplaats = Marktplaats.new
-    @items = @marktplaats.get_category("Videokaarten")
   end
   
-  it "returns a list of items available on marktplaats" do
-    expect(@items).to have_at_least(100).items
+  describe "#get_category" do
+    before do
+      @items = @marktplaats.get_category("Videokaarten")
+    end
+
+    it "returns a list of items available on marktplaats" do
+      expect(@items).to have_at_least(100).items
+    end
+
+    it "has details on all items" do
+      @items.each do |item|
+        expect(item.title).not_to be_nil
+        expect(item.price).not_to be_nil
+        expect(item.url).not_to be_nil
+        expect(item.image_url).not_to be_nil
+      end
+    end
   end
 
-  it "has details on all items" do
-    @items.each do |item|
-      expect(item.title).not_to be_nil
-      expect(item.price).not_to be_nil
-      expect(item.url).not_to be_nil
-      expect(item.image_url).not_to be_nil
+  describe "#search" do
+    it "returns a list of items for a search term" do
+      search_term = "playstation 3 120 gb"
+      results = @marktplaats.search(search_term)
+
+      expect(results).to have_at_least(5).items
     end
   end
 
