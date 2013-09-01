f65ad7ee220a6b793c29357464937ad72db6ca5a
Pieter Kuijpers <pieter@pi-q.nl>
2013-09-01 12:33:58 +0200

    Extracted class SearchResultPage

--- lib/marktplaats/command.rb
+++ lib/marktplaats/command.rb
@@ -1,4 +1,5 @@
 require_relative 'categories'
+require_relative 'search_result_page'
 
 require 'pry'
 
@@ -106,50 +107,8 @@ module Marktplaats
     def get_items(url, max_results)
       agent = Mechanize.new
       # Load first page
-      page = agent.get(url)
-      items = items_from(page)[0..(max_results - 1)]
-
-      next_button = page.link_with(:text => 'Volgende')
-      until next_button.attributes[:class].include?("disabled") or items.size >= max_results
-        page = next_button.click
-        items_to_add = (max_results - items.length - 1).abs
-        items.concat(items_from(page)[0..(items_to_add)])
-        next_button = page.link_with(:text => 'Volgende')
-      end
-      items
-    end
-
-    def items_from(page)
-      listings = page.search('.search-result').to_ary
-
-      # Bottom listing contains advertisements, don't include them
-      listings.reject! do |listing|
-        listing.attributes['class'].value.include?('bottom-listing')
-      end
-
-      # Ignore professional sellers: items that have a 'seller-link'
-      listings.reject! do |listing|
-        listing.at('.seller-link') != nil
-      end
-
-      listings.map do |listing|
-        title = listing.at('.mp-listing-title').text
-        price = to_price(listing.at('.price').text)
-        url = listing.at('.listing-title-description a').attributes['href'].value[/(.*)\?/, 1]
-        image_url = 'http:' + listing.at('.listing-image img').attributes['src'].value
-        date = listing.at('.column-date').text.strip
-        {:title => title, :url => url, :image_url => image_url, :date_posted => date, :price => price}
-      end
-    end
-
-    def to_price(price_text)
-      # Strip €, spaces, non-breaking space from price and convert to number
-      price = price_text.gsub(/[€\s\u00A0.]/, '').gsub(/,/, '.').to_f
-      if price > 0
-        price
-      else
-        price_text
-      end
+      page = SearchResultPage.new(agent.get(url))
+      page.items(max_results)
     end
 
   end
